0e1f6688cdbc5bd3893c8e3d44d88aebb391aa06
Ricardo Alencar <ricfilho0007@gmail.com>
2023-12-23 14:04:16 -0300

    feat: enable cors and add '/' route

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/ricsjs/infohub-api/api/routes"
@@ -15,6 +16,9 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(cors.Default())
+
 	routes.SetupRoutes(r)
 
 	r.Run(":8080")
